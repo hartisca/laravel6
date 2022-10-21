9ad88b220f0edac7fa950be644378e477133c9c4
hector1 <hearca@fp.insjoaquimmir.cat>
2022-10-21 19:01:29 +0200

    Afegit nou canvi a web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -35,3 +35,6 @@ Route::get('/', function (Request $request) {
     return view('welcome');
  });
  
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');

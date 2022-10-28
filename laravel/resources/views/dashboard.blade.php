88e70d88ab01bb2f65fb9bc5d1493e423e1c0a3b
hector-artiso <hearca@fp.insjoaquimmir.cat>
2022-10-28 18:54:02 +0200

    Modificats auth/register de users per tornar a posar rol 1 en default i botó del dashboard

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -15,7 +15,7 @@
         </div>
     </div>
     @section('content')
-   <h2>{{ __('Resources') }}</h2>
-   <a href="{{ url('/files') }}">{{ __('Files') }}</a>
+   <h2>{{ __('Resources') }}</h2>   
+   <a href="{{ url('/files') }}" class="btn btn-primary">{{ __('Files') }}</a>
     @endsection
 </x-app-layout>

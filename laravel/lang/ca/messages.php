26791c85e21c7f121eb894ce05fd45294b26657c
hector-artiso <hearca@fp.insjoaquimmir.cat>
2022-11-22 16:22:07 +0100

    Push perq el marc pugui fer algo

---
+++ laravel/lang/ca/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'success'=>'El :attribute s\'ha desat correctament',
+    'error'=>'Error al desar el :attribute',
+    'file' => 'fitxer',
+    'File' => 'Fitxer',
+
+
+
+];
---
+++ laravel/lang/en/messages.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'success'=>':attribute successfully saved',
+    'error'=>'Error saving the :attribute',
+    'file' => 'file',
+    'File' => 'File',
+
+
+];

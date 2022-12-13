9793f34ddc2d6f494d22447215a75a81aef3b9bb
hector-artiso <hearca@fp.insjoaquimmir.cat>
2022-12-13 17:13:46 +0100

    Push abans de subbranques

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\FileController;
+use App\Http\Controllers\Api\TokenController;
+use App\Http\Controllers\Api\User;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('files', FileController::class);
+
+Route::post('files/{file}', [FileController::class, 'update_workaround']);
+
+Route::get('/user', [TokenController::class, 'user'])->middleware('auth:sanctum');
+
+Route::post('/register', [TokenController::class, 'register']);
+
+Route::post('/login', [TokenController::class, 'login']);
+
+Route::post('/logout', [TokenController::class, 'logout'])->middleware('auth:sanctum');
+
+
+
+

8da31fd4413ac0bbf27b3ba4078764da480cfee8
Marc-albero <maalpa@fp.insjoaquimmir.cat>
2023-01-31 15:41:53 +0100

    checkout fet

--- laravel/resources/views/contacte/contacte.blade.php
+++ laravel/resources/views/contacte/contacte.blade.php
@@ -15,13 +15,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
  
 
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-     integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
-     crossorigin=""/>
-
-     <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
-     integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
-     crossorigin=""></script>
 
     <!-- Scripts -->
     @env(['local','development'])
@@ -132,15 +125,8 @@
     </div> 
 </div>
 <br>
-    <div id="map">
-        <script>
-            var map = L.map('map').setView([51.505, -0.09], 13);
-
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
-        </script>
+    <div >
+       
     </div>
     
 </body>
@@ -201,8 +187,5 @@
   color: black;
 } 
 
-#map {
-    height: 200px;
-    
-}
+
 </style>

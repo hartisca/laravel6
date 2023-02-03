010b455ecc32745b6061c2aff7ed6972597d4f2f
Marc-albero <maalpa@fp.insjoaquimmir.cat>
2023-02-03 16:46:43 +0100

    ...

--- laravel/resources/views/contacte/contacte.blade.php
+++ laravel/resources/views/contacte/contacte.blade.php
@@ -120,13 +120,19 @@
        <h1>Vols visitar-nos?</h1>
 
         <h2>ubica'ns al mapa</h2>
-        
-    
     </div> 
 </div>
 <br>
-    <div >
-       
+    
+    <div id="map">
+        <script>
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+</script>
     </div>
     
 </body>

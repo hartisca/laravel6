01a79802c1f24dd01821768d994247edd971828d
Marc-albero <maalpa@fp.insjoaquimmir.cat>
2023-02-13 18:42:35 +0100

    hector mira aixo

--- laravel/resources/views/contacte/contacte.blade.php
+++ laravel/resources/views/contacte/contacte.blade.php
@@ -135,7 +135,7 @@
                 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                }).addTo(map);
+                }).addTo(map);    
             </script> 
         
         </div>
